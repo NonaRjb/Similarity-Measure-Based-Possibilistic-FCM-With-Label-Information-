8b58b3221b31cfe1fafb30ac8e90dab2a48d6b1f
Nona_Rjb <rajabi_nona@ee.sharif.edu>
2020-07-26 15:20:05 +0430

    Writing The Code For Calculation Of Tau Is Finished "simMeasure.m" has 6 functions to compute d, dis, W, rho, S and tau. there is still a missing part in calculation of rho in which d_lk < w should be taken into consideration.

--- simMeasure.m
+++ simMeasure.m
@@ -1,20 +1,70 @@
-function S = simMeasure(data, cluster_n, a, b, U, T, img_size)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+function tau = simMeasure(data, cluster_n, U, T, m, eta, a, b, img_size)
+%SIMMEASURE measures the interclass and intraclass similarity
+%   tau = simMeasure(data, cluster_n, U, T, m, eta, a, b, img_size)
+%   measures interclass and intraclass similarity for each data point in
+%   a cluster. 
+%   tau(ik) = sum( rho(jk) * S(ij) ); j = 1 : C
+
+if nargin < 9
+    rho = rel_sim(data, U, T, m, eta, a, b);
+    S = similarity(data, cluster_n, U, T, m, eta, a, b);
+else
+    rho = rel_sim(data, U, T, m, eta, a, b, img_size);
+    S = similarity(data, cluster_n, U, T, m, eta, a, b, img_size);
+end
+
+tau = S * rho;
 
 end
 
-function S = similarity(data, U, T, m, eta, a, b, img_size)
+function S = similarity(data, cluster_n, U, T, m, eta, a, b, img_size)
+
+if nargin < 9
+    rho = rel_sim(data, U, T, m, eta, a, b, img_size);
+else
+    rho = rel_sim(data, U, T, m, eta, a, b);
+end
+
+max_vals = max(rho, [], 2);
+indices = [];
+% flag = zeros(1, size(rho, 1));
+% pre_flag = 0;
+
+for i = 1 : size(rho, 1)
+    indices = [indices, find(rho(i, :) == max_vals(i))];
+    % flag(1, i) = length(indices) + pre_flag;
+    % pre_flag = flag(1, i);
+end
+points = unique(indices);
+
+S = zeros(cluster_n, cluster_n);
+for i = 1 : cluster_n
+    for j = i : cluster_n
+        num = 0;
+        den1 = 0;
+        den2 = 0;
+        for k = points
+            num = num + rho(i, k) * rho(j, k);
+            den1 = den1 + rho(i, k);
+            den2 = den2 + rho(j, k);
+        end
+        den = den1 * den2;
+        den = sqrt(den);
+        S(i, j) = num / den;
+    end
+end
+
 end
 
 function rho = rel_sim(data, U, T, m , eta, a, b, img_size)
-if nargin < 2
+
+if nargin < 8
     W = point_sim(data);
 else
     W = point_sim(data, img_size);
 end
 den = sum(a * U .^ m + b * T .^ eta, 2);
-rho = (a * U .^ m + b * T .^ eta) * W ./ den; % todo: d_lk<w is not considered
+rho = (a * U .^ m + b * T .^ eta) * W ./ den; % todo: d_lk < w is not considered
 end
 
 function W = point_sim(data, img_size)
@@ -35,7 +85,10 @@ for l = 1 : size(data, 1)
         d(l, k) = norm(data(l) - data(k));
     end
 end
-d = d + d'; % todo: still not sure about what H is in the paper (should be devided by H)
+if max(d(:)) > 0
+    d = d / max(d(:)); % todo: still not sure about what H is in the paper (should be devided by H)
+end
+d = d + d'; 
 end
 
 function dis = coordinate_dist(data, img_size)
