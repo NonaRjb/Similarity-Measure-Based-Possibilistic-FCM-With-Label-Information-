57bc3a0f6781881747d6e2986d51aa43f0628db2
Nona_Rjb <rajabi_nona@ee.sharif.edu>
2020-08-05 10:42:51 +0430

    Main.m is Modified to Accept Different Kind of Inputs By setting the parameters at the beginning of "main.m", it can accept arbitrary image or and image from IBSR brain mri dataset or 2d data points .txt file.

--- main.m
+++ main.m
@@ -2,22 +2,42 @@ clc
 close all 
 clear variables
 
-img = imread('Mri2.bmp');
-img = imresize(img, 0.5);
-[m, n] = size(img);
-img_vec = reshape(im2double(img), [], 1);
-
-mat = txt2mat('pathbased.txt');
-
-options = [NaN, NaN, 3, 1, 0, NaN, 5, NaN, m, n]; 
+filepath = 'Mri2.bmp';
+is_image = 1;
+is_IBSR = 0;
 cluster_n = 5;
-[U, T, obj_fcn] = sim_pfcm_l(img_vec, cluster_n, options);
-[~, i] = max(U, [], 1);
-figure
-imshow(reshape(i, m, n), [])
-% figure
-% gscatter(mat(:,1), mat(:,2), i')
-% for k = 1 : cluster_n
-%     figure
-%     imshow(reshape(U(k,:), m, n))
-% end
+
+if is_image == 1
+    if is_IBSR == 1
+        n = 10;
+        seg_file = './20Normals_T1_seg/';
+        [img_raw, seg_raw] = IBSRread(seg_file, filepath, n);
+        img = img_raw(:, :, 20);
+        seg = seg_raw(:, :, 20); 
+        [m, n] = size(img);
+        img_vec = reshape(im2double(img), [], 1);
+        options = [NaN, NaN, NaN, NaN, NaN, NaN, 5, NaN, m, n];
+        [U, T, obj_fcn] = sim_pfcm_l(img_vec, cluster_n, options);
+        [~, i] = max(U, [], 1);
+        figure
+        imshow(reshape(i, m, n), [])
+    else
+        img = imread(filepath);
+        img = imresize(img, 0.5);
+        [m, n] = size(img);
+        img_vec = reshape(im2double(img), [], 1);
+        options = [NaN, NaN, NaN, NaN, NaN, NaN, 5, NaN, m, n];
+        [U, T, obj_fcn] = sim_pfcm_l(img_vec, cluster_n, options);
+        [~, i] = max(U, [], 1);
+        figure
+        imshow(reshape(i, m, n), [])
+    end
+else
+    mat = txt2mat(filepath);
+    options = [NaN, NaN, NaN, NaN, NaN, NaN, 5, NaN, 0, 0];
+    [U, T, obj_fcn] = sim_pfcm_l(mat(:, 1:2), cluster_n, options);
+    [~, i] = max(U, [], 1);
+    figure
+    gscatter(mat(:,1), mat(:,2), i')
+end
+
